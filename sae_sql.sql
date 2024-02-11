8507ba3275127c8572c632072fd92ae223096b2e
Olivier Maraval <olivier.maraval@edu.univ-fcomte.fr>
2024-02-11 10:14:50 +0100

    Updated sql with new db, added two views to help with website's queries

--- sae_sql.sql
+++ sae_sql.sql
@@ -14,7 +14,7 @@ declinaison_meuble,
 meuble,
 type_meuble,
 materiau;
--- MATERIAU
+-- Tables
 CREATE TABLE IF NOT EXISTS materiau(
     id_materiau INT AUTO_INCREMENT,
     libelle_materiau VARCHAR(255),
@@ -43,7 +43,6 @@ VALUES ('Sheesham massif'),
     ('Eucalyptus'),
     ('Chêne clair'),
     ('Chêne foncé');
--- TYPE_MEUBLE
 CREATE TABLE IF NOT EXISTS type_meuble(
     id_type_meuble INT AUTO_INCREMENT,
     libelle_type_meuble VARCHAR(255),
@@ -57,7 +56,6 @@ VALUES ('Étagère'),
     ('Vitrine'),
     ('Chaise'),
     ('Pouf');
--- MEUBLE
 CREATE TABLE IF NOT EXISTS meuble(
     id_meuble INT AUTO_INCREMENT,
     nom_meuble VARCHAR(255),
@@ -237,7 +235,6 @@ VALUES (
         '43.jpg',
         7
     );
--- DECLINAISON_MEUBLE
 CREATE TABLE IF NOT EXISTS declinaison_meuble(
     id_declinaison_meuble INT AUTO_INCREMENT,
     stock INT,
@@ -257,46 +254,45 @@ INSERT INTO declinaison_meuble (
         materiau_id
     )
 VALUES (1, 819, '1.jpg', 8, 1),
-    (2, 419, '2.jpg', 2, 1),
+    (2, 419.25, '2.jpg', 2, 1),
     (3, 799, '3.jpg', 3, 1),
-    (4, 976, '4.jpg', 12, 2),
+    (4, 976.50, '4.jpg', 12, 2),
     (4, 1427, '7.jpg', 1, 16),
     (4, 1725, '10.jpg', 6, 17),
     (5, 700, '13.jpg', 13, 3),
     (6, 345, '14.jpg', 12, 3),
     (7, 518, '15.jpg', 2, 3),
     (8, 159, '16.jpg', 2, 17),
-    (8, 159, '17.jpg', 5, 18),
+    (8, 159.50, '17.jpg', 5, 18),
     (9, 226, '18.jpg', 3, 4),
     (10, 56, '19.jpg', 25, 5),
     (10, 56, '20.jpg', 16, 6),
-    (10, 56, '21.jpg', 11, 8),
-    (10, 56, '22.jpg', 7, 9),
+    (10, 57, '21.jpg', 11, 8),
+    (10, 56.99, '22.jpg', 7, 9),
     (11, 65, '23.jpg', 7, 8),
-    (11, 65, '24.jpg', 4, 9),
-    (11, 65, '25.jpg', 2, 10),
-    (11, 65, '26.jpg', 1, 11),
-    (11, 65, '27.jpg', 5, 12),
+    (11, 65.20, '24.jpg', 4, 9),
+    (11, 64, '25.jpg', 2, 10),
+    (11, 66, '26.jpg', 1, 11),
+    (11, 66, '27.jpg', 5, 12),
     (11, 65, '28.jpg', 6, 13),
-    (12, 895, '29.jpg', 21, 17),
+    (12, 895.99, '29.jpg', 21, 17),
     (13, 950, '30.jpg', 2, 17),
-    (14, 1450, '31.jpg', 5, 19),
+    (14, 1450.25, '31.jpg', 5, 19),
     (15, 425, '32.jpg', 6, 19),
     (16, 2250, '33.jpg', 7, 20),
     (17, 1400, '34.jpg', 16, 20),
     (18, 750, '35.jpg', 23, 17),
     (18, 750, '36.jpg', 2, 17),
-    (19, 75, '38.jpg', 0, 12),
+    (19, 76, '38.jpg', 0, 12),
     (19, 75, '39.jpg', 5, 11),
-    (19, 75, '41.jpg', 6, 7),
+    (19, 76, '41.jpg', 6, 7),
     (19, 75, '42.jpg', 0, 9),
-    (20, 150, '43.jpg', 12, 10),
-    (20, 150, '44.jpg', 11, 9),
-    (20, 150, '45.jpg', 8, 14),
-    (20, 150, '46.jpg', 9, 15),
-    (20, 150, '47.jpg', 19, 8),
-    (20, 150, '48.jpg', 27, 6);
--- UTILISATEUR
+    (20, 151, '43.jpg', 12, 10),
+    (20, 150.25, '44.jpg', 11, 9),
+    (20, 152.99, '45.jpg', 8, 14),
+    (20, 152.99, '46.jpg', 9, 15),
+    (20, 152.99, '47.jpg', 19, 8),
+    (20, 152.99, '48.jpg', 27, 6);
 CREATE TABLE IF NOT EXISTS utilisateur(
     id_utilisateur INT AUTO_INCREMENT,
     login VARCHAR(255),
@@ -322,7 +318,7 @@ VALUES (
         'admin@admin.fr',
         'pbkdf2:sha256:600000$828ij7RCZN24IWfq$3dbd14ea15999e9f5e340fe88278a45c1f41901ee6b2f56f320bf1fa6adb933d',
         'ROLE_admin',
-        'admin',
+        'Administrateur',
         '1'
     ),
     (
@@ -331,7 +327,7 @@ VALUES (
         'client@client.fr',
         'pbkdf2:sha256:600000$ik00jnCw52CsLSlr$9ac8f694a800bca6ee25de2ea2db9e5e0dac3f8b25b47336e8f4ef9b3de189f4',
         'ROLE_client',
-        'client',
+        "Semih Remork",
         '1'
     ),
     (
@@ -340,10 +336,9 @@ VALUES (
         'client2@client2.fr',
         'pbkdf2:sha256:600000$3YgdGN0QUT1jjZVN$baa9787abd4decedc328ed56d86939ce816c756ff6d94f4e4191ffc9bf357348',
         'ROLE_client',
-        'client2',
+        "Jack Séparou",
         '1'
     );
--- ADRESSE
 CREATE TABLE IF NOT EXISTS adresse(
     id_adresse INT AUTO_INCREMENT,
     nom_adresse VARCHAR(255),
@@ -382,7 +377,6 @@ VALUES (
         '41 Av. du Général Leclerc',
         1
     );
--- CONCERNE
 CREATE TABLE IF NOT EXISTS concerne(
     utilisateur_id INT,
     adresse_id INT,
@@ -395,7 +389,6 @@ VALUES (1, 1),
     (2, 2),
     (2, 3),
     (3, 4);
--- ETAT
 CREATE TABLE IF NOT EXISTS etat(
     id_etat INT AUTO_INCREMENT,
     libelle_etat VARCHAR(255),
@@ -406,7 +399,6 @@ VALUES ('En attente'),
     ('Expédié'),
     ('Validé'),
     ('Confirmé');
--- COMMANDE
 CREATE TABLE IF NOT EXISTS commande(
     id_commande INT AUTO_INCREMENT,
     date_achat DATETIME,
@@ -424,7 +416,6 @@ VALUES ('2024-01-01', 2, 1, 2),
     ('2024-01-03', 3, 2, 4),
     ('2024-01-04', 3, 3, 4),
     ('2023-03-03', 2, 4, 2);
--- LIGNE_COMMANDE
 CREATE TABLE IF NOT EXISTS ligne_commande(
     commande_id INT,
     declinaison_meuble_id INT,
@@ -435,68 +426,58 @@ CREATE TABLE IF NOT EXISTS ligne_commande(
     FOREIGN KEY(declinaison_meuble_id) REFERENCES declinaison_meuble(id_declinaison_meuble)
 );
 INSERT INTO ligne_commande (
-        commande_id,
         declinaison_meuble_id,
+        commande_id,
         quantite_lc,
         prix_lc
     )
-INSERT INTO ligne_commande (declinaison_meuble_id, commande_id, quantite_lc, prix_lc)
-VALUES (2, 1, 1, ),
-       (1, 1, 2,),
-       (3, 2, 3,),
-       (1, 3, 1,),
-       (1, 4, 11,),
-       (2, 4, 5,),
-       (3, 4, 4,),
-       (4, 4, 12,),
-       (5, 4, 6,),
-       (6, 4, 6,),
-       (7, 4, 1,),
-       (8, 4, 2,),
-       (9, 4, 2,),
-       (10, 4, 6,),
-       (11, 4, 9,),
-       (12, 4, 13,),
-       (13, 4, 13,),
-       (14, 4, 12,),
-       (15, 4, 2,),
-       (16, 4, 2,),
-       (17, 4, 5,),
-       (18, 4, 3,),
-       (19, 4, 25,),
-       (20, 4, 16,),
-       (21, 4, 11,),
-       (22, 4, 7,),
-       (23, 4, 7,),
-       (24, 4, 4,),
-       (25, 4, 2,),
-       (26, 4, 1,),
-       (27, 4, 5,),
-       (28, 4, 6,),
-       (29, 4, 21,),
-       (30, 4, 2,),
-       (31, 4, 5,),
-       (32, 4, 6,),
-       (33, 4, 7,),
-       (34, 4, 16,),
-       (35, 4, 23,),
-       (36, 4, 2,),
-       (38, 4, 5,),
-       (39, 4, 6,),
-       (41, 4, 12,),
-       (42, 4, 11,),
-       (43, 4, 8,),
-       (44, 4, 9,),
-       (45, 4, 19,),
-       (46, 4, 27,),
-       (1, 5, 2,),
-       (2, 5, 1,),
-       (3, 5, 3,);
-
-
-UPDATE ligne_commande
-SET prix = (SELECT prix_meuble FROM meuble WHERE meuble.id_meuble = ligne_commande.meuble_id);
--- COMMENTAIRE
+VALUES (2, 1, 1, 419),
+    (1, 1, 2, 819),
+    (3, 2, 3, 799),
+    (1, 3, 1, 819),
+    (1, 4, 11, 819),
+    (2, 4, 5, 419),
+    (3, 4, 4, 799),
+    (4, 4, 12, 976),
+    (5, 4, 6, 1427),
+    (6, 4, 6, 1725),
+    (7, 4, 1, 700),
+    (8, 4, 2, 345),
+    (9, 4, 2, 518),
+    (10, 4, 6, 159),
+    (11, 4, 9, 159),
+    (12, 4, 13, 226),
+    (13, 4, 13, 56),
+    (14, 4, 12, 57),
+    (15, 4, 2, 58),
+    (16, 4, 2, 62),
+    (17, 4, 5, 65),
+    (18, 4, 3, 65.99),
+    (19, 4, 25, 62),
+    (20, 4, 16, 64.99),
+    (21, 4, 11, 64.99),
+    (22, 4, 7, 65),
+    (23, 4, 7, 895.25),
+    (24, 4, 4, 950),
+    (25, 4, 2, 1450),
+    (26, 4, 1, 425),
+    (27, 4, 5, 2250),
+    (28, 4, 6, 1400),
+    (29, 4, 21, 750),
+    (30, 4, 2, 750),
+    (31, 4, 5, 75),
+    (32, 4, 6, 75.45),
+    (33, 4, 7, 72.10),
+    (34, 4, 16, 73),
+    (35, 4, 23, 150),
+    (36, 4, 2, 145),
+    (38, 4, 5, 145),
+    (39, 4, 6, 155),
+    (1, 5, 2, 819),
+    (2, 5, 1, 419),
+    (3, 5, 3, 799),
+    (25, 5, 2, 1445),
+    (34, 5, 11, 75.25);
 CREATE TABLE IF NOT EXISTS commentaire(
     id_commentaire INT AUTO_INCREMENT,
     commentaire VARCHAR(255),
@@ -508,7 +489,6 @@ CREATE TABLE IF NOT EXISTS commentaire(
     FOREIGN KEY(meuble_id) REFERENCES meuble(id_meuble),
     FOREIGN KEY(utilisateur_id) REFERENCES utilisateur(id_utilisateur)
 );
--- HISTORIQUE
 CREATE TABLE IF NOT EXISTS historique(
     id_historique INT AUTO_INCREMENT,
     date_consultation DATETIME NOT NULL,
@@ -518,7 +498,6 @@ CREATE TABLE IF NOT EXISTS historique(
     FOREIGN KEY(meuble_id) REFERENCES meuble(id_meuble),
     FOREIGN KEY(utilisateur_id) REFERENCES utilisateur(id_utilisateur)
 );
--- LISTE_ENVIE
 CREATE TABLE IF NOT EXISTS liste_envie(
     id_le INT AUTO_INCREMENT,
     date_update DATETIME NOT NULL,
@@ -528,7 +507,6 @@ CREATE TABLE IF NOT EXISTS liste_envie(
     FOREIGN KEY(meuble_id) REFERENCES meuble(id_meuble),
     FOREIGN KEY(utilisateur_id) REFERENCES utilisateur(id_utilisateur)
 );
--- FACTURER
 CREATE TABLE IF NOT EXISTS facturer(
     utilisateur_id INT,
     adresse_id INT,
@@ -536,17 +514,26 @@ CREATE TABLE IF NOT EXISTS facturer(
     FOREIGN KEY(utilisateur_id) REFERENCES utilisateur(id_utilisateur),
     FOREIGN KEY(adresse_id) REFERENCES adresse(id_adresse)
 );
--- LIGNE_PANIER
 CREATE TABLE IF NOT EXISTS ligne_panier(
     utilisateur_id INT,
     declinaison_meuble_id INT,
-    date_ajout INT,
+    date_ajout DATETIME,
     quantite_lp INT,
     PRIMARY KEY(utilisateur_id, declinaison_meuble_id),
     FOREIGN KEY(utilisateur_id) REFERENCES utilisateur(id_utilisateur),
     FOREIGN KEY(declinaison_meuble_id) REFERENCES declinaison_meuble(id_declinaison_meuble)
 );
--- NOTE
+INSERT INTO ligne_panier (
+        declinaison_meuble_id,
+        utilisateur_id,
+        quantite_lp,
+        date_ajout
+    )
+VALUES (1, 1, 2, '2024-01-01'),
+    (3, 1, 1, '2024-01-01'),
+    (2, 2, 3, '2024-01-02'),
+    (1, 2, 3, '2024-01-02'),
+    (1, 3, 1, '2024-01-03');
 CREATE TABLE IF NOT EXISTS note(
     utilisateur_id INT,
     meuble_id INT,
@@ -555,3 +542,15 @@ CREATE TABLE IF NOT EXISTS note(
     FOREIGN KEY(utilisateur_id) REFERENCES utilisateur(id_utilisateur),
     FOREIGN KEY(meuble_id) REFERENCES meuble(id_meuble)
 );
+-- Views
+CREATE view IF NOT EXISTS v_ligne_commande AS
+SELECT *
+FROM ligne_commande
+    JOIN declinaison_meuble ON declinaison_meuble_id = id_declinaison_meuble
+    JOIN commande ON commande_id = id_commande
+    JOIN utilisateur ON utilisateur_id = id_utilisateur;
+CREATE view IF NOT EXISTS v_ligne_panier AS
+SELECT *
+FROM ligne_panier
+    JOIN declinaison_meuble ON declinaison_meuble_id = id_declinaison_meuble
+    JOIN utilisateur ON utilisateur_id = id_utilisateur;
