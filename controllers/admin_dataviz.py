cd048a8dfcaf71c66926f3a7a53b7399edc5efda
YahiaKh12 <floodaimerairire@gmail.com>
2024-03-28 05:20:23 +0100

    Commentaire/note (CLIENT) ok

--- controllers/admin_dataviz.py
+++ controllers/admin_dataviz.py
@@ -12,6 +12,9 @@ admin_dataviz = Blueprint('admin_dataviz', __name__,
 def show_type_meuble_stock():
     mycursor = get_db().cursor()
     sql = '''
+        SELECT * 
+        FROM commentaire
+        
     
            '''
     # mycursor.execute(sql)
